af573a729a0ba409f3e70de99a992a11d9f2b0ac
Alex Petkovic <>
2019-02-04 23:59:48 -0500

    Make blinky blink all the LEDs

--- app/test_blinky/src/main.cpp
+++ app/test_blinky/src/main.cpp
@@ -1,12 +1,38 @@
 #include "mbed.h"
 
-DigitalOut myled(LED1);
+DigitalOut ledA(LED1);
+DigitalOut ledB(LED2);
+DigitalOut ledC(LED3);
+DigitalOut ledD(LED4);
 
 int main() {
+	int count = 0;
+
     while(1) {
-        myled = 1;
-        wait(0.2);
-        myled = 0;
-        wait(0.2);
+
+    	// Trigger the four LEDs one after the other
+    	switch (count) {
+    		case 0: 
+    			ledA = 1;
+    			ledD = 0;
+    			break;
+    		case 1:
+    			ledB = 1;
+    			ledA = 0;
+    			break;
+    		case 2:
+    			ledC = 1;
+    			ledB = 0;
+                break;
+    		case 3:
+    			ledD = 1;
+    			ledC = 0;
+    			count = -1;
+    			break;
+    	}
+
+    	count++;
+
+        wait(0.5);
     }
 }
