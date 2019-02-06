17d05b6c11b4c6f8203266d623ef8f020717c9bd
Alex Petkovic <>
2019-02-05 23:28:17 -0500

    cleanup

--- app/test_blinky/src/main.cpp
+++ app/test_blinky/src/main.cpp
@@ -6,32 +6,32 @@ DigitalOut ledC(LED3);
 DigitalOut ledD(LED4);
 
 int main() {
-	int count = 0;
+    int count = 0;
 
     while(1) {
 
-    	// Trigger the four LEDs one after the other
-    	switch (count) {
-    		case 1: 
-    			ledA = 1;
-    			ledD = 0;
-    			break;
-    		case 2:
-    			ledB = 1;
-    			ledA = 0;
-    			break;
-    		case 3:
-    			ledC = 1;
-    			ledB = 0;
-                	break;
-    		case 4:
-    			ledD = 1;
-    			ledC = 0;
-    			count = 0;
-    			break;
-    	}
+        // Trigger the four LEDs one after the other
+        switch (count) {
+            case 1: 
+                ledA = 1;
+                ledD = 0;
+                break;
+            case 2:
+                ledB = 1;
+                ledA = 0;
+                break;
+            case 3:
+                ledC = 1;
+                ledB = 0;
+                    break;
+            case 4:
+                ledD = 1;
+                ledC = 0;
+                count = 0;
+                break;
+        }
 
-    	count++;
+        count++;
         wait(0.5);
     }
 }
