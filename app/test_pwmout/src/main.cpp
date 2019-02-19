9d8b4ee8053319ed0befca68d9bb74e541bef0aa
Alex Petkovic <>
2019-02-19 18:13:00 -0500

    Improve PWM output test

--- app/test_pwmout/src/main.cpp
+++ app/test_pwmout/src/main.cpp
@@ -1,31 +1,32 @@
 #include "mbed.h"
  
-PwmOut led(LED1);
-PwmOut pwm1(PWM1);
+DigitalOut led(LED1);
+PwmOut 	   pwm1(PWM1);
  
 int main() {
 
-	float period = 4.0f;
-    float duty = 0.5f;
+	int period_ms = 1; // Equivalent to 1kHz frequency
+    float duty = 0.0f;
 
     // Specify PWM period
-    led.period(period);  // 4 second period
-    pwm1.period(period);
+    pwm1.period_ms(period_ms);
 
     // Slowly increase the duty cycle. Once it is 100%, set back to 0%.
     while(1) {
-    	if (duty > 1.0f)
+    	if (duty > 1.001f)
     	{
     		duty = 0.0f;
     	}
 
     	// Set the duty cycle on the pins
-    	led.write(duty);
     	pwm1.write(duty);
+    	printf("pwm set to %.2f %%\n", pwm1.read() * 100);
 
     	// Increment the duty cycle
-    	// duty += 0.1f;
+    	duty += 0.1f;
 
-    	wait(0.5);
+    	led = !led;
+
+    	wait(1);
     }
 }
