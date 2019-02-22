0f3d651961f6eca91b6f93643ae6c184fdfcd6d6
Alex Petkovic <>
2019-02-22 10:38:34 -0500

    Start testing positional PID

--- app/test_pid_position_pwm/src/main.cpp
+++ app/test_pid_position_pwm/src/main.cpp
@@ -1,31 +1,78 @@
+#include "mbed.h"
 #include "PID.h"
+#include "PwmIn.h"
  
-#define RATE 0.1
+// Constants
+const float kUpdatePeriod = 0.05;
+
+Serial pc(SERIAL_TX, SERIAL_RX);
+
+PwmOut motor(MOTOR3);
+DigitalOut motorDirection(MOTOR3_DIR);
+PwmIn pwmEncoder(ENC_A1, 12);
+
+DigitalOut led(LED1);
+
+PID velocityPIDController(0.35, 0.0, 0.0, kUpdatePeriod);
+Timer timer;
+
+// Variables
+float motorPWMDuty = 0.0;
+float encoderPWMDuty = 0.0;
+float prevEncoderPWMDuty = 0.0;
+float angularVelocity = 0.0;
+
+// Velocity to reach.
+float angularVelocityGoal = 100;
+
+// Initialize motor
+void initializeMotor(void){
+    motor.period_ms(1); // 1 ms period = 1 kHz frequency
+    motor.write(0.0);
+    motorDirection = 1;
+}
  
-//Kc, Ti, Td, interval
-PID controller(1.0, 0.0, 0.0, RATE);
-AnalogIn pv(p15);
-PwmOut   co(p26);
+// Setup velocity PID controller
+void initializePidController(void){
+    velocityPIDController.setInputLimits(0.0, 200.0);
+    velocityPIDController.setOutputLimits(0.0, 1.0);
+    velocityPIDController.setBias(0.0);
+    velocityPIDController.setMode(PID_AUTO_MODE);
+}
  
-int main(){
+int main() {
  
-  //Analog input from 0.0 to 3.3V
-  controller.setInputLimits(0.0, 3.3);
-  //Pwm output from 0.0 to 1.0
-  controller.setOutputLimits(0.0, 1.0);
-  //If there's a bias.
-  controller.setBias(0.3);
-  controller.setMode(AUTO);
-  //We want the process variable to be 1.7V
-  controller.setSetPoint(1.7);
+    // Initialization
+    initializeMotor();
+    initializePidController();
+
+    wait(1);
+
+    // Set velocity set point.
+    velocityPIDController.setSetPoint(angularVelocityGoal);
+
+    timer.start();
  
-  while(1){
-    //Update the process variable.
-    controller.setProcessValue(pv.read());
-    //Set the new output.
-    co = controller.getRealOutput();
-    //Wait for another loop calculation.
-    wait(RATE);
-  }
+    // Run for 3 seconds.
+    while (true) {
+        // Get the encoder PWM duty and calculate the angular velocity
+        encoderPWMDuty = pwmEncoder.avgDutyCycle();
+        angularVelocity = 360.0 * (encoderPWMDuty - prevEncoderPWMDuty) / timer.read(); // Degrees per second
+        timer.reset();
+        prevEncoderPWMDuty = encoderPWMDuty;
+
+        // Update the PID controller
+        velocityPIDController.setProcessValue(angularVelocity);
+        motorPWMDuty = velocityPIDController.compute();
+        motor.write(motorPWMDuty);
+
+        pc.printf("%f, %f, %f\r\n", angularVelocity, angularVelocityGoal, motorPWMDuty);
+
+        wait(kUpdatePeriod);
+        led = !led;
+    }
  
+    // Stop motors
+    motor.write(0.0);
+    
 }
