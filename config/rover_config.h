bba4d9169f9568d25bc509bce67c98b3932c9cbe
Alex Petkovic <>
2019-02-24 19:24:37 -0500

    Change CAN IDs

--- config/rover_config.h
+++ config/rover_config.h
@@ -7,11 +7,11 @@
 // CAN bus config
 #define ROVER_CANBUS_FREQUENCY	1000000 // 1 Mbps
 #define ROVER_CANID_FILTER_MASK 0xF00   // Use bits 8:11 for addressing and 0:7 for command/message type
-#define ROVER_JETSON_CANID   	0x100
-#define ROVER_SAFETY_CANID		0x200
+#define ROVER_SAFETY_CANID		0x100
+#define ROVER_SCIENCE_CANID		0x200
 #define ROVER_ARMO_CANID		0x300
 #define ROVER_ARM1_CANID		0x400
-#define ROVER_SCIENCE_CANID		0x500
+#define ROVER_JETSON_CANID   	0x500
 
 // Controls
 #define ROVER_MOTOR_PWM_FREQ_HZ 1000    // 1 kHz
