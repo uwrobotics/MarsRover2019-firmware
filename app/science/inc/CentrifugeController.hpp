cdfefa0f2fd16a034cf4f263db3b1d12fb3ff1be
Jack MacKinnon <jack_mackinnon@hotmail.com>
2019-05-16 22:38:06 -0400

    Science Firmware v1.1 Changes made in accordance to comments and discussion with Alex P.

--- app/science/inc/CentrifugeController.hpp
+++ app/science/inc/CentrifugeController.hpp
@@ -15,17 +15,23 @@ typedef enum t_centrifugeControlMode {
 
 typedef struct{
     // Motor Configuration
-    PinName     centrifugeMotor;
-    const float maxMotorSpeed = 200; //In RPM, need to be rad/s?
+    PinName centrifugeMotor;
 
     // Motor Direction Configuration
     PinName centrifugeMotorDir;
 
+    // Encoder Configuration
+    PinName centrifugeEncoder;
+
     // PID Configuration
     float PIDUpdateInterval;
-    float centrifugePID_P;
-    float centrifugePID_I;
-    float centrifugePID_D;
+    float positionPID_P;
+    float positionPID_I;
+    float positionPID_D;
+
+    // Max and min PWM signals for motor unit speed
+    float PWM_MIN_SIGNAL = -1.0;
+    float PWM_MAX_SIGNAL = 1.0;
 
 } t_centrifugeControllerConfig;
 
@@ -33,16 +39,21 @@ class CentrifugeController{
     private:
         t_centrifugeControlMode      m_centrifugeControlMode;
         t_centrifugeControllerConfig m_centrifugeControllerConfig;
-        const int                    LIMIT_SWITCH_ANGLE_OFFSET = 15;
+        const int                    LIMIT_SWITCH_ANGLE_OFFSET = 15; // Value in degrees
     public:
         CentrifugeController( t_centrifugeControllerConfig m_centrifugeControllerConfig,
                               t_centrifugeControlMode      m_centrifugeControlMode );
 
         void  setControlMode( t_centrifugeControlMode );
         void  setCentrifugeMotorSpeed( float setSpeed );
+
+        // tube_num within range [0-11]
+        // Places tube of # tube_num under the Auger output mouth
+        void  setCentrifugePosition( int tube_num );
+
         float getAngle(); // Returns angle tube_0 is from under Auger mouth
 
-        void Reset(); // Restores initial position of centrifuge (ie. tube_0 under Auger mouth)
+        void resetToTube0(); // Restores initial position of centrifuge (ie. tube_0 under Auger mouth)
 };
 
 #endif // CENTRIFUGE_CONTROLLER_H
