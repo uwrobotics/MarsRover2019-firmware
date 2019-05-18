210a89fc24f02b15ae3fd9d3dab6698f26e3f3fc
Alex Petkovic <16639190+jetkov@users.noreply.github.com>
2019-05-18 01:27:15 -0700

    Init claw controller

---
+++ app/arm_upper/inc/ArmClawController.h
@@ -0,0 +1,100 @@
+#ifndef ARM_CLAW_CONTROLLER_H
+#define ARM_CLAW_CONTROLLER_H
+
+/* Controller for the arm claw
+ */
+
+#include "mbed.h"
+#include "Motor.h"
+#include "PwmIn.h"
+#include "PID.h"
+#include "PinNames.h"
+
+// TYPES
+
+typedef struct {
+    PinName pwmPin;
+    PinName dirPin;
+    bool inverted;
+
+} t_motorConfig;
+
+typedef struct {
+    PinName pwmPin;
+    float zeroAngleDutyCycle;
+    float minAngleDegrees;
+    float maxAngleDegrees;
+    bool inverted;
+} t_absoluteEncoderConfig;
+
+typedef struct {
+    float P, I, D, bias;
+} t_pidConstants;
+
+typedef struct {
+    // Joint motor config
+    t_motorConfig motor;
+
+    // Joint encoder config
+    t_absoluteEncoderConfig encoder;
+
+    // PID config
+    t_pidConstants velocityPID, positionPID;
+
+    float initPIDUpdateInterval;
+    float PIDInputVelocityMinDegPerSec, PIDInputVelocityMaxDegPerSec;
+    float PIDOutputMotorMinDutyCycle, PIDOutputMotorMaxDutyCycle;
+
+} t_armJointConfig;
+
+typedef enum t_controlMode {
+    motorDutyCycle,
+    velocityPID,
+    positionPID
+
+} t_jointControlMode;
+
+// CLASS
+
+class ArmClawController {
+
+public:
+
+    explicit ArmClawController(t_armJointConfig armJointConfig, t_jointControlMode controlMode = velocityPID);
+
+    mbed_error_status_t setControlMode(t_jointControlMode controlMode);
+
+    mbed_error_status_t setMotorSpeedPercent(float speedPercent);
+
+    mbed_error_status_t setVelocityDegreesPerSec(float velocityDegreesPerSec);
+
+    mbed_error_status_t setAngleDegrees(float angleDegrees);
+
+    t_jointControlMode getControlMode();
+
+    float getAngleDegrees();
+
+    float getAngleVelocityDegreesPerSec();
+
+    void update();
+
+protected:
+
+    void initializePIDControllers(void);
+
+    t_jointControlMode m_controlMode;
+    t_armJointConfig m_armJointConfig;
+
+    Motor m_motor;
+    PwmIn m_encoder;
+
+    PID m_velocityPIDController;
+    PID m_positionPIDController;
+
+    float m_inversionMultiplier;
+
+    Timer timer;
+
+};
+
+#endif // ARM_CLAW_CONTROLLER_H
