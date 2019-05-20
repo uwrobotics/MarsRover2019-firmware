031682ded51f42b1a4141069b7250563100ca357
Alex Petkovic <16639190+jetkov@users.noreply.github.com>
2019-05-20 02:32:39 -0700

    Add upper arm main program

--- app/arm_upper/src/main.cpp
+++ app/arm_upper/src/main.cpp
@@ -1,38 +1,310 @@
-#include "PinNames.h"
 #include "mbed.h"
+#include "rover_config.h"
+#include "CANMsg.h"
+#include "PwmIn.h"
+#include "PID.h"
+#include "Motor.h"
+#include "ArmWristController.h"
+#include "ArmClawController.h"
 
-DigitalOut ledA(LED1);
-DigitalOut ledB(LED2);
-DigitalOut ledC(LED3);
-DigitalOut ledD(LED4);
+const ArmWristController::t_armWristConfig wristConfig = {
+        .leftJointConfig = {
+                .motor = {
+                        .pwmPin = MOTOR1,
+                        .dirPin = MOTOR1_DIR,
+                        .inverted = true
+                },
 
-int main() {
-    int count = 0;
+                .encoder = {
+                        .pwmPin = ENC_A1,
+                        .zeroAngleDutyCycle = 0.50f,
+                        .minAngleDegrees = -162.0f,
+                        .maxAngleDegrees = 162.0f,
+                        .inverted = true
+                },
 
-    while(1) {
+                .velocityPID = {
+                        .P    = 0.15f,
+                        .I    = 0.45f,
+                        .D    = 0.0f,
+                        .bias = 0.0f,
+                        .interval = 0.05f
+                },
 
-        // Trigger the four LEDs one after the other
-        switch (count) {
-            case 1:
-                ledA = 1;
-                ledD = 0;
-                break;
-            case 2:
-                ledB = 1;
-                ledA = 0;
-                break;
-            case 3:
-                ledC = 1;
-                ledB = 0;
-                break;
-            case 4:
-                ledD = 1;
-                ledC = 0;
-                count = 0;
-                break;
-        }
+                .positionPID = {
+                        .P    = 5.5f,
+                        .I    = 0.0f,
+                        .D    = 0.0f,
+                        .bias = 0.0f,
+                        .interval = 0.05f
+                },
 
-        count++;
-        wait(0.5);
+                .minInputVelocityDegPerSec = -20.0f,
+                .maxInputVelocityDegPerSec = 20.0f,
+                .minOutputMotorDutyCycle = -1.0f,
+                .maxOutputMotorDutyCycle = 1.0f
+        },
+
+        .rightJointConfig = {
+                .motor = {
+                        .pwmPin = MOTOR2,
+                        .dirPin = MOTOR2_DIR,
+                        .inverted = false
+                },
+
+                .encoder = {
+                        .pwmPin = ENC_A2,
+                        .zeroAngleDutyCycle = 0.50f,
+                        .minAngleDegrees = -162.0f,
+                        .maxAngleDegrees = 162.0f,
+                        .inverted = false
+                },
+
+                .velocityPID = {
+                        .P    = 0.3f,
+                        .I    = 0.2f,
+                        .D    = 0.0f,
+                        .bias = 0.0f,
+                        .interval = 0.05f
+                },
+
+                .positionPID = {
+                        .P    = 15.7f,
+                        .I    = 0.0f,
+                        .D    = 0.0f,
+                        .bias = 0.0f,
+                        .interval = 0.05f
+                },
+
+                .minInputVelocityDegPerSec = -20.0f,
+                .maxInputVelocityDegPerSec = 20.0f,
+                .minOutputMotorDutyCycle = -1.0f,
+                .maxOutputMotorDutyCycle = 1.0f
+        },
+
+        .leftToRightMotorBias = 0.0f
+};
+
+const ArmClawController::t_clawConfig clawConfig = {
+        .motor = {
+                .pwmPin   = MOTOR3,
+                .dirPin   = MOTOR3_DIR,
+                .inverted = true
+        },
+
+        .encoder = {
+                .channelAPin = ENCR1_CH1,
+                .channelAPin = ENCR1_CH2,
+                .indexPin    = ENCR1_INDEX,
+
+                .pulsesPerRevolution = 360,
+                .encoding = QEI::X4_ENCODING,
+                .inverted = false
+        },
+
+        .limitSwitchPin = LIM_1A, // CHECK
+
+        .calibrationDutyCycle = 0.2f,
+        .calibrationTimeoutSeconds = 7.0f,
+
+        .positionPID = {
+                .P = 5.0f,
+                .I = 0.0f,
+                .D = 0.0f,
+
+                .bias     = 0.0f,
+                .interval = 0.05f
+        },
+
+        .minInputSeparationDistanceCm = 0.0f,
+        .maxInputSeparationDistanceCm = 120.0f,
+
+        .minOutputMotorDutyCycle = -1.0f,
+        .maxOutputMotorDutyCycle = 1.0f
+
+};
+
+
+Serial             pc(SERIAL_TX, SERIAL_RX, ROVER_DEFAULT_BAUD_RATE);
+CAN                can(CAN_RX, CAN_TX, ROVER_CANBUS_FREQUENCY);
+CANMsg             rxMsg;
+CANMsg             txMsg;
+
+DigitalOut         ledErr(LED1);
+DigitalOut         ledCAN(LED4);
+
+ArmWristController wristController(wristConfig, ArmJointController::velocityPID);
+ArmClawController  clawController(clawConfig, ArmClawController::positionPID);
+
+Timer              canSendTimer;
+
+void printCANMsg(CANMessage& msg) {
+    pc.printf("  ID      = 0x%.3x\r\n", msg.id);
+    pc.printf("  Type    = %d\r\n", msg.type);
+    pc.printf("  Format  = %d\r\n", msg.format);
+    pc.printf("  Length  = %d\r\n", msg.len);
+    pc.printf("  Data    =");
+    for(int i = 0; i < msg.len; i++)
+        pc.printf(" 0x%.2X", msg.data[i]);
+    pc.printf("\r\n");
+}
+
+enum armCommand {
+
+    setWristControlMode = ROVER_ARM_UPPER_CANID,
+    setWristPitchMotion,
+    setWristRollMotion,
+    setClawControlMode,
+    setClawMotion,
+
+    firstCommand = setWristControlMode,
+    lastCommand  = setClawMotion
+
+};
+
+void initCAN() {
+    can.filter(ROVER_ARM_UPPER_CANID, ROVER_CANID_FILTER_MASK, CANStandard);
+}
+
+ArmJointController::t_jointControlMode handleSetWristControlMode(CANMsg *p_newMsg) {
+    ArmJointController::t_jointControlMode controlMode;
+    *p_newMsg >> controlMode;
+
+    MBED_ASSERT_SUCCESS(wristController.setControlMode(controlMode));
+
+    return controlMode;
+}
+
+ArmClawController::t_clawControlMode handleSetClawControlMode(CANMsg *p_newMsg) {
+    ArmClawController::t_clawControlMode controlMode;
+    *p_newMsg >> controlMode;
+
+    MBED_ASSERT_SUCCESS(clawController.setControlMode(controlMode));
+
+    return controlMode;
+}
+
+float handleSetWristPitchMotion(CANMsg *p_newMsg) {
+    float motionData = 0;
+    *p_newMsg >> motionData;
+
+    ArmJointController::t_jointControlMode controlMode = wristController.getControlMode();
+
+    switch (controlMode) {
+
+        case ArmJointController::motorDutyCycle:
+            MBED_ASSERT_SUCCESS(wristController.setPitchSpeedPercent(motionData));
+            break;
+
+        case ArmJointController::velocityPID:
+            MBED_ASSERT_SUCCESS(wristController.setPitchVelocityDegreesPerSec(motionData));
+            break;
+
+        case ArmJointController::positionPID:
+            MBED_ASSERT_SUCCESS(wristController.setPitchAngleDegrees(motionData));
+            break;
+    }
+
+}
+
+float handleSetWristRollMotion(CANMsg *p_newMsg) {
+    float motionData = 0;
+    *p_newMsg >> motionData;
+
+    ArmJointController::t_jointControlMode controlMode = wristController.getControlMode();
+
+    switch (controlMode) {
+
+        case ArmJointController::motorDutyCycle:
+            MBED_ASSERT_SUCCESS(wristController.setRollSpeedPercent(motionData));
+            break;
+
+        case ArmJointController::velocityPID:
+            MBED_ASSERT_SUCCESS(wristController.setRollVelocityDegreesPerSec(motionData));
+            break;
+
+        case ArmJointController::positionPID:
+            MBED_ASSERT_SUCCESS(wristController.setRollAngleDegrees(motionData));
+            break;
+    }
+
+}
+
+float handleSetClawMotion(CANMsg *p_newMsg) {
+    float motionData = 0;
+    *p_newMsg >> motionData;
+
+    ArmClawController::t_clawControlMode controlMode = clawController.getControlMode();
+
+    switch (controlMode) {
+
+        case ArmClawController::motorDutyCycle:
+            MBED_ASSERT_SUCCESS(clawController.setMotorSpeedPercent(motionData));
+            break;
+
+        case ArmClawController::positionPID:
+            MBED_ASSERT_SUCCESS(clawController.setSeparationDistanceCm(motionData));
+            break;
     }
 }
+
+void processCANMsg(CANMsg *p_newMsg) {
+    switch (p_newMsg->id) {
+
+        case setWristControlMode:
+            handleSetWristControlMode(p_newMsg);
+            break;
+
+        case setWristPitchMotion:
+            handleSetWristPitchMotion(p_newMsg);
+            break;
+
+        case setWristRollMotion:
+            handleSetWristRollMotion(p_newMsg);
+
+        case setClawControlMode:
+            handleSetClawControlMode(p_newMsg);
+            break;
+
+        case setClawMotion:
+            handleSetClawMotion(p_newMsg);
+
+        default:
+            pc.printf("Recieved unimplemented command\r\n");
+            break;
+    }
+}
+
+void sendJetsonInfo() {
+    
+}
+
+int main(void)
+{
+    pc.printf("Program Started\r\n\r\n");
+
+    initCAN();
+
+    clawController.runEndpointCalibration();
+
+    canSendTimer.start();
+
+    while (1) {
+
+        if (can.read(rxMsg)) {
+            processCANMsg(&rxMsg);
+            rxMsg.clear();
+            ledCAN = !ledCAN;
+        }
+
+        if (canSendTimer.read() > 0.1) {
+            sendJetsonInfo();
+            canSendTimer.reset();
+        }
+
+        wristController.update();
+        clawController.update();
+
+    }
+}
+
