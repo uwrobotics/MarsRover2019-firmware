302691bf06e8dccd9a8b388f427b59e1e70a21e8
qinyang <q7bao@edu.uwaterloo.ca>
2019-05-20 18:11:01 -0400

    initial implementation, still needs to test CAN communication

--- app/test_neopixel/src/main.cpp
+++ app/test_neopixel/src/main.cpp
@@ -2,62 +2,63 @@
 // and https://wp.josh.com/2014/05/13/ws2812-neopixels-are-not-so-finicky-once-you-get-to-know-them/
 
 #include "mbed.h"
-#include "neopixel.h"
+#include "CANMsg.h"
+#include "rover_config.h"
 
 #define REPEAT_FIVE(a)     a;a;a;a;a
 #define REPEAT_TEN(a)      REPEAT_FIVE(a); REPEAT_FIVE(a)
 
-neopixel::neopixel(PinName signalPin, int pixel_num):led_signal(signalPin) {
-	m_pixel_num = pixel_num;
+// CPU cycle timing
+#define NS_PER_SEC (1000000000L) 
+#define CYCLES_PER_SEC (48000000.0)
+#define NS_PER_CYCLE ( NS_PER_SEC / CYCLES_PER_SEC )
 
-	pin_mask = gpio_set(signalPin);
+#define NS_TO_CYCLES(n) ( int((n) / NS_PER_CYCLE / 6.0 ))
 
-	uint32_t port_index = STM_PORT(signalPin);
-	switch (port_index) {
-	case PortA:
-		pin_gpio = (GPIO_TypeDef *)(GPIOA_BASE);
-		break;
-	case PortB:
-		pin_gpio = (GPIO_TypeDef *)(GPIOB_BASE);
-		break;
-	case PortC:
-		pin_gpio = (GPIO_TypeDef *)(GPIOC_BASE);
-		break;
-	}
+// Timings for 0 and 1 bit
+#define T1H  900    // Width of a 1 bit in ns
+#define T1L  600    // Width of a 1 bit in ns
 
-	pin_reg_set = &pin_gpio->BSRR;
-	pin_reg_clr = &pin_gpio->BRR;
-}
+#define T0H  400    // Width of a 0 bit in ns
+#define T0L  900    // Width of a 0 bit in ns
 
-inline void neopixel::sendBit(int val) {
+#define RES 6000    // Width of the low gap between bits to cause a frame to latch
+
+#ifdef NUCLEO_PINMAP
+DigitalOut			led_signal(PA_5);
+#else
+DigitalOut			led_signal(PC_0);
+CAN                 can(CAN_RX, CAN_TX, ROVER_CANBUS_FREQUENCY);
+CANMsg              rxMsg;
+const unsigned int  RX_ID = 0x210;
+const unsigned int  GREEN = 1;
+const unsigned int  ORANGE = 2;
+const unsigned int  RED = 3;
+#endif
+
+const int			m_pixel_num = 10;
+
+inline void sendBit(int val) {
 
 	if (val) {
-		//*pin_reg_set |= pin_mask;
 		led_signal = 1;
-		wait(0.5);
 		REPEAT_TEN(__NOP());
 		REPEAT_TEN(__NOP());
 		REPEAT_TEN(__NOP());
 		REPEAT_TEN(__NOP());
+
 		led_signal = 0;
-		wait(0.5);
-		//*pin_reg_clr |= pin_mask;
 		REPEAT_TEN(__NOP());
 		REPEAT_TEN(__NOP());
 		REPEAT_FIVE(__NOP());
 	}
 	else {
-		//*pin_reg_set = pin_mask;
 		led_signal = 1;
-		wait(0.5);
 		REPEAT_TEN(__NOP());
-		REPEAT_FIVE(__NOP());
 		__NOP();
 		__NOP();
 
-		//*pin_reg_clr = pin_mask;
 		led_signal = 0;
-		wait(0.5);
 		REPEAT_TEN(__NOP());
 		REPEAT_TEN(__NOP());
 		REPEAT_TEN(__NOP());
@@ -66,7 +67,7 @@ inline void neopixel::sendBit(int val) {
 	
 }
 
-inline void neopixel::sendByte(unsigned char byte) {
+inline void sendByte(unsigned char byte) {
 
 	for (unsigned char bit = 0; bit < 8; bit++) {
 
@@ -76,7 +77,7 @@ inline void neopixel::sendByte(unsigned char byte) {
 	}
 }
 
-inline void neopixel::sendPixel(unsigned char r, unsigned char g, unsigned char b) {
+inline void sendPixel(unsigned char r, unsigned char g, unsigned char b) {
 
 	sendByte(g);          // Neopixel needs colors in green then red then blue order
 	sendByte(r);
@@ -85,35 +86,46 @@ inline void neopixel::sendPixel(unsigned char r, unsigned char g, unsigned char 
 }
 
 // Just wait long enough without sending any bits will cause the pixels to latch and display the last sent frame
-void neopixel::show() {
+void show() {
 	wait_us((RES / 1000UL) + 1);  // Round up since the delay must be _at_least_ this long (too short might not work, too long not a problem)
 }
 
-void neopixel::showColor(unsigned char r, unsigned char g, unsigned char b) {
+void showColor(unsigned char r, unsigned char g, unsigned char b) {
 	for (int p = 0; p < m_pixel_num; p++) {
 		sendPixel(r, g, b);
 	}
 	show();
 }
 
+
 int main(void) {
-	neopixel neo(PA_5, 10);
-	//DigitalOut led_signal(PA_5);
-	//unsigned int pin_mask = gpio_set(PA_5);
-	//GPIO_TypeDef* pin_gpio = (GPIO_TypeDef *)(GPIOA_BASE);
-	//__IO uint32_t* pin_reg_set = &pin_gpio->BSRR;
-	//__IO uint32_t* pin_reg_clr = &pin_gpio->BRR;
 
 	while (1) {
-	//	led_signal = 1;
-	//	wait(0.5);
-	//	led_signal = 0;
-	//	wait(0.5);
-		neo.sendBit(1);
-	//	neo.showColor(0, 0, 255);
-	//	*pin_reg_set = pin_mask;
-	//	wait(1);
-	//	*pin_reg_clr = pin_mask;
-	//	wait(1);
+#ifdef ROVERBOARD_SCIENCE_PINMAP
+		if (can.read(rxMsg)) {
+			// Filtering performed by software:
+			if (rxMsg.id == RX_ID) {
+				int color_state = rxMsg.data[0];
+				switch (color_state) {
+				case GREEN:
+					showColor(0, 255, 0);
+					break;
+				case ORANGE:
+					showColor(255, 127, 80);
+					break;
+				case RED:
+					showColor(255, 0, 0);
+					break;
+				}
+			}
+		}
+#else
+		showColor(255, 0, 0);
+		wait(1);
+		showColor(0, 255, 0);
+		wait(1);
+		showColor(255, 127, 80);
+		wait(1);
+#endif
 	}
 }
