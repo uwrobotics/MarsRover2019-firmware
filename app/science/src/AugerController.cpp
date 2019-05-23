246aac0feae3b3e5253688dd8328f7e68558595b
Jack MacKinnon <jack_mackinnon@hotmail.com>
2019-05-22 22:51:37 -0400

    Refactoring the classes to include QEI objects - interface is more alike ArmClawController

--- app/science/src/AugerController.cpp
+++ app/science/src/AugerController.cpp
@@ -9,8 +9,7 @@
 #include "PinNames.h"
 
 AugerController::AugerController( t_augerControllerConfig controllerConfig )
-:   m_augerControlMode( motorDutyCycle ),
-    m_augerControllerConfig( controllerConfig ),
+:   m_augerControllerConfig( controllerConfig ),
     m_motor( controllerConfig.motor.pwmPin, controllerConfig.motor.dirPin, controllerConfig.motor.inverted ),
     m_encoder( controllerConfig.encoder.pwmPin )
 {
@@ -20,27 +19,11 @@ AugerController::AugerController( t_augerControllerConfig controllerConfig )
     else {
         m_inversionMultiplier = 1;
     }
-
-    timer.start();
 }
 
 // Takes a float between -1 <= percent <= 1
 mbed_error_status_t AugerController::setMotorSpeedPercent( float percent )
 {
-    // Only handling valid motor speeds
-    if ( percent < -1.0f || percent > 1.0f ) {
-        percent = 0.0f;
-        // TODO: RETURN A BAD mbed_error_status_t IF BAD INPUT?
-    }
     m_motor.speed( percent );
     return MBED_SUCCESS;
-}
-
-//TODO: ADJUST FUNCTION
-void AugerController::update()
-{
-    float interval = timer.read();
-    timer.reset();
-
-    // TODO: MOTOR CAN ALWAYS FREELY SPIN, SO NO EXTRA LOGIC?
 }
