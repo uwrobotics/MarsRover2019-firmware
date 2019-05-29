79a4b0767bfdacb63b4c00b078b57d64e1756b37
Maggie Zhang <xujiazhang310@gmail.com>
2019-05-29 14:09:02 -0400

    Voltage monitor added to safety

--- app/safety/src/main.cpp
+++ app/safety/src/main.cpp
@@ -52,9 +52,11 @@ const float compression_factor = 0.110629;
 const int cell_num = 6;
 const float full_cell = 4.20;
 const float low_cell = 3.75; // TODO verify this value
-const float full_bat = full_cell * cell_num * compression_factor/3.3;
-const float low_bat = low_cell * cell_num * compression_factor/3.3;
+const float full_bat = full_cell * cell_num;
+const float low_bat = low_cell * cell_num;
+const float board_voltage = 3.3;
 float bat_value = 0;
+float bat_value_raw = 0;
 #define V_INDEX 3
 
 void initCAN() {
@@ -117,14 +119,23 @@ int main() {
         // Reading of battery voltage
         /* Battery voltage conversion 
         * Voltage divider of 82k and 10.2k provide a compression factor of 0.110629
+		* 10.19k, 81.8k
         * Does CAN Msg need to be sent out?
+        * 400 mV when 5V is on
+        * 0.16 reading
         */
-       bat_value = battery.read();
+       bat_value_raw = battery.read();
+       bat_value = bat_value_raw * board_voltage/ compression_factor;
        pc.printf("Battery Level: %f\r\n", bat_value);
        if (bat_value < low_cell){
-           CANMsg txMsg(TX_ID + V_INDEX);
+           // CANMsg txMsg(TX_ID + V_INDEX);
            pc.printf("REPLACE BATTERY, Battery Level LOW!!! \r\n");
        }
+
+        pc.printf("Hello World! %d\r\n", i);
+        i++;
+        wait(0.5);
+        led = i % 2;
 		wait(1);
     }
 }
