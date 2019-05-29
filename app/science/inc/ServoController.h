31d259d3bb6ebc7dd8106e08a66c1412704d6391
Alex Petkovic <16639190+jetkov@users.noreply.github.com>
2019-05-29 17:38:39 -0600

    Science cleanup and improvements
    - cleanup
    - fixed bugs
    - added serve libs

---
+++ app/science/inc/ServoController.h
@@ -0,0 +1,33 @@
+#ifndef AUGER_CONTROLLER_H
+#define AUGER_CONTROLLER_H
+// Controller for the Auger
+
+#include "mbed.h"
+#include "Motor.h"
+#include "PwmIn.h"
+#include "PID.h"
+#include "PinNames.h"
+
+class AugerController {
+
+    public:
+
+        typedef struct {
+
+            // Motor Configuration
+            Motor::t_motorConfig motor;
+
+        } t_augerConfig;
+        
+        AugerController( t_augerConfig controllerConfig );
+                         
+        mbed_error_status_t setMotorDutyCycle(float percent);
+
+    private:
+
+        t_augerConfig m_augerConfig;
+        Motor m_motor;
+};
+
+
+#endif // AUGER_CONTROLLER_H
