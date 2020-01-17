85881ddf66b4fa5a2afdc97fdc8fc2006ba8a167
yreda <yreda@edu.uwaterloo.ca>
2020-01-17 18:08:44 -0500

    [-] Younes training updated
    - Implemented feedback and code now compiles

--- app/tutorial_servo_control_can/src/main.cpp
+++ app/tutorial_servo_control_can/src/main.cpp
@@ -1,17 +1,18 @@
-#include "CANMsg.h"
-#include "rover_config.h"
 #include "mbed.h"
+#include "rover_config.h"
+#include "CANMsg.h"
 #include "TutorialServo.h"
 
 int main(){
     TutorialServo myServo(PA_1);
-    CAN           can(CAN_RX, CAN_TX, ROVER_CANBUS_FREQUENCY);
+    CAN           can(PB_8, PB_9, ROVER_CANBUS_FREQUENCY);
     CANMsg        rxMsg;
 
     while(1){
         //assume the percentage looks like 0.5
         if (can.read(rxMsg)){
-            float percentage = rxMsg.data[0];
+            float percentage = 0;
+            rxMsg >> percentage;
             float angle = myServo.getServoRangeInDegrees() * percentage;
             myServo.setPositionInDegrees(angle);
         }
--- lib/user/tutorial_servo/src/TutorialServo.cpp
+++ lib/user/tutorial_servo/src/TutorialServo.cpp
@@ -1,33 +1,6 @@
-#ifndef TUTORIAL_SERVO_H
-#define TUTORIAL_SERVO_H
-
-class TutorialServo {
-public:
-   // Constructor: Takes a servo pin name (ex. PA_1), and optionally a servo range
-   // that has a default value of 180.0 degrees, a minimum pulsewidth of 1ms, and a     
-   // maximum pulsewidth of 2ms.
-   TutorialServo(PinName servoPin, float servoRangeInDegrees = 180.0, 
-                 float minPulsewidthInMs = 1, float maxPulsewidthInMs = 2); 
-
-   // Set servo range (ex. 270 deg)
-   void setAngleRangeInDegrees(float degrees); 
-
-   // Set servo pulse width range (ex. 1ms, 2ms
-   void setPulsewidthRangeInMs(float minPulsewidthMs, float maxPulsewidthMs);
-
-   // Set servo position (ex. 45 deg)
-   void setPositionInDegrees(float degrees); 
-
-    // I needed this function for the second part of the challenge
-   // Get the servo range in degrees
-   float getServoRangeInDegrees();
-
-private:
-   float  m_servoRangeInDegrees;
-   PwmOut m_servoPwmOut;
-   float m_minPulsewidthInMs;
-   float m_maxPulsewidthInMs;
-};
+#include "mbed.h"
+#include "rover_config.h"
+#include <TutorialServo.h>
 
 TutorialServo::TutorialServo(PinName servoPin, float servoRangeInDegrees, float minPulsewidthInMs, float maxPulsewidthInMs) 
                             : m_servoPwmOut(servoPin){
@@ -61,5 +34,3 @@ void TutorialServo::setPositionInDegrees(float degrees){
 float TutorialServo::getServoRangeInDegrees(){
     return m_servoRangeInDegrees;
 }
-
-#endif
